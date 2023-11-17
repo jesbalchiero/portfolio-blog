94e16415241a5761ab0c38c087d2b2fca0edca91
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2023-11-16 21:01:59 -0300

    Adding Header and Footer

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>Hello, Nuxt 3!</h1>
+  <div class="flex flex-col min-h-screen">
+    <Header></Header>
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+      <NuxtPage />
+    </main>
+    <Footer></Footer>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">Hello, I'm Jean Sbalchiero!</h1>
+        <p class="text-base text-gray-900 p-2 italic">Full Stack Web Developer in Conecta Softwares</p>
+    </section>
+    <section class="flex flex-col md:flex-row">
+        <div class="md:w-3/4">
+            <h2 class="text-3xl font-bold mt-8">My Story</h2>
+            <p class="text-lg py-2">
+
+            </p>
+        </div>
+        <!-- <img src="~/assets/images/jean-avatar.png" alt="Profile Avatar" class="w-1/2 md:max-w-sm p-8 mx-auto"> -->
+    </section>
+    <section>
+        <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
